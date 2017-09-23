406027d11602646b3bfe566de4ccdba5f0bb5c12
FABIAN <31972011+fabianlopez@users.noreply.github.com>
2017-09-23 20:34:53 -0300

    cambio
    palabra "algo" en clase "clase"

---
+++ Proyecto Final/Clase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Proyecto_Final
+{
+    public class Clase
+    {
+        public int Id;
+        public string Nombre;
+        public string Descripción;
+
+        public Clase ( int id, string nombre, string descripción)
+        {
+            this.Id = id;
+            this.Nombre = nombre;
+            this.Descripción = descripción;
+
+            algo
+
+
+
+        }
+    }
+}
