34120038bf7e3d16710d886b85e6c78325d87fb1
FABIAN <31972011+fabianlopez@users.noreply.github.com>
2017-09-23 20:39:16 -0300

    A VER AHORA
    cambio grosso

---
+++ borrar/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace borrar
+{
+    public class Class1
+    {
+        ESTO ES SOLO UNA PRUEBA
+
+    }
+}
